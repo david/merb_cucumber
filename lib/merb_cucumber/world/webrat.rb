c531723d3d89b6108af26e6b500f4e4fee7dbf12
Roman Gonzalez <roman@hasmanydevelopers.com>
2008-10-19 17:54:24 -0430

    Changing the Webrat::RackSession to a Webrat::Session, this is because Webrat::RackSession is not on the webrat gem specification (yet)

--- lib/merb_cucumber/world/webrat.rb
+++ lib/merb_cucumber/world/webrat.rb
@@ -4,8 +4,21 @@ require File.join(File.dirname(__FILE__), 'base')
 module Merb
   module Test
     module World
-      class Webrat < Webrat::RackSession
+      # We are going to wait for the Webrat::RackSession to be included on the gem spec of webrat
+      class Webrat < Webrat::Session
         include Base
+        
+        ## Begin RackSession implementations
+        
+        def response_body
+          @response.body
+        end
+
+        def response_code
+          @response.status
+        end
+        
+        ## End of RackSession implementation
         
         attr_reader :response
 
