76c5610a063bac09c55004cee4dbc14d8b714bed
David Leal <dgleal@gmail.com>
2008-10-19 10:16:17 +0100

    This doesn't belong here.

--- lib/merb_cucumber.rb
+++ lib/merb_cucumber.rb
@@ -1,9 +1,3 @@
 if defined?(Merb::Plugins)
   Merb::Plugins.add_rakefiles "merb_cucumber" / "merbtasks"
-  
-  if Merb.environment == "test"
-    Merb::BootLoader.after_app_loads do
-      require File.join(File.dirname(__FILE__) / "merb_cucumber" / "feature")
-    end
-  end 
 end
