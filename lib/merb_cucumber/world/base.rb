19d1dad606024d355a605c91b55489bca9e8aa54
Roman Gonzalez <roman@hasmanydevelopers.com>
2008-10-20 01:16:25 -0430

    Adding the annonymous modules that have matchers on the Basic World

--- lib/merb_cucumber/world/base.rb
+++ lib/merb_cucumber/world/base.rb
@@ -5,6 +5,10 @@ module Merb
         include Merb::Test::ControllerHelper
         include Merb::Test::RouteHelper
         include Merb::Test::ViewHelper
+        # Including the RequestMatchers (They are annonymous modules, so we have to do it the "hacker way!")
+        Merb::Test::ExampleGroup.included_modules.each do |extension| 
+          include extension if extension.name.empty?
+        end
       end
     end
   end
