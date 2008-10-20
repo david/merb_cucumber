e3f59f980146f15ba32f3f08b79c79bd42d9f699
Roman Gonzalez <roman@hasmanydevelopers.com>
2008-10-19 21:09:08 -0430

    Removing printing traces from the datamapper helper

--- lib/merb_cucumber/helpers/datamapper.rb
+++ lib/merb_cucumber/helpers/datamapper.rb
@@ -9,7 +9,6 @@ module Merb
 
           $main.Before do
             # Getting all the adapters configured in DataMapper
-            puts "\x1B[44mStarting a transaction for each repository\x1B[0m"
             @__transactions__ = ::DataMapper::Repository.adapters.values.map do |adapter|
               transaction = ::DataMapper::Transaction.new(adapter)
               transaction.begin
@@ -18,7 +17,6 @@ module Merb
           end
 
           $main.After do
-            puts "\x1B[41mDoing a Rollback of the transactions\x1B[0m"
             @__transactions__.each { |transaction| transaction.rollback }
           end
 
