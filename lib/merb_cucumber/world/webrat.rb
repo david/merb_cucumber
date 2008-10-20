bcd3eff87b5c843cad2569cebd05e164cdd9e0a3
Roman Gonzalez <roman@hasmanydevelopers.com>
2008-10-20 11:52:48 -0430

    Removing some ActionController dependencies from webrat

--- lib/merb_cucumber/world/webrat.rb
+++ lib/merb_cucumber/world/webrat.rb
@@ -1,6 +1,27 @@
 # require 'webrat/rack/rack_session'
 require File.join(File.dirname(__FILE__), 'base')
 
+# We have to change the Webrat::Field implementation (Some Rails dependencies!)
+module Webrat
+  class Field
+    
+    def to_param
+      return nil if disabled?
+      value = @value.to_s.gsub('&', '%26')
+      param_parser.query_parse("#{name}=#{value}")
+    end
+    
+    def param_parser
+      Merb::Request
+    end
+    
+    def test_uploaded_file
+      raise Merb::StatusCodes::NotImplemented.new
+    end
+    
+  end
+end
+
 module Merb
   module Test
     module World
