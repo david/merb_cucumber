bcbb4a1b58171a791c74a18be15aa2d918877bdc
Roman Gonzalez <roman@hasmanydevelopers.com>
2008-10-24 12:40:17 -0430

    Adding some changes to the code (found some bugs and I'm fixing them)

--- lib/merb_cucumber/world/base.rb
+++ lib/merb_cucumber/world/base.rb
@@ -2,8 +2,6 @@ module Merb
   module Test
     module World
       module Base
-        include Merb::Test::Matchers
-
         include Merb::Test::ControllerHelper
         include Merb::Test::RouteHelper
         include Merb::Test::ViewHelper
--- lib/merb_cucumber/world/webrat.rb
+++ lib/merb_cucumber/world/webrat.rb
@@ -1,31 +1,10 @@
-require 'webrat/merb/merb_session'
+require 'webrat/merb'
 require File.join(File.dirname(__FILE__), 'base')
-
-# We have to change the Webrat::Field implementation (Some Rails dependencies!)
-module Webrat
-  class Field
-    
-    def to_param
-      return nil if disabled?
-      value = @value.to_s.gsub('&', '%26')
-      param_parser.query_parse("#{name}=#{value}")
-    end
-    
-    def param_parser
-      Merb::Request
-    end
-    
-    def test_uploaded_file
-      raise Merb::StatusCodes::NotImplemented.new
-    end
-    
-  end
-end
 
 module Merb
   module Test
     module World
-      class Webrat < ::Webrat::MerbSession
+      class Webrat < ::Webrat::Session
         include Base
       end
     end
