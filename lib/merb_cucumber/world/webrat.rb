ab32c796db925bd24e24a121a7b23abb618ddd95
David Leal <dgleal@gmail.com>
2008-10-24 16:30:31 +0100

    Moved webrat functionality to webrat.

--- lib/merb_cucumber/world/webrat.rb
+++ lib/merb_cucumber/world/webrat.rb
@@ -1,39 +1,11 @@
-require 'webrat/rack/rack_session'
+require 'webrat/merb/merb_session'
 require File.join(File.dirname(__FILE__), 'base')
 
 module Merb
   module Test
     module World
-      class Webrat < Webrat::RackSession
+      class Webrat < ::Webrat::MerbSession
         include Base
-        
-        attr_reader :response
-
-        %w(get head post put delete).each do |verb|
-          define_method(verb) do |*args| # (path, data, headers = nil)
-            path, data, headers = *args
-            all = (headers || {})
-            all.merge!(:method => "#{verb.upcase}") unless all[:method] || all["REQUEST_METHOD"]
-            
-            unless data.empty?
-              if verb == "post"
-                all.merge!(:body_params => data)
-              elsif verb == "get"
-                all.merge!(:params => data)
-              end
-            end
-
-            @response = request(path, all)
-            
-            class << @response
-              def body
-                super.to_s
-              end
-            end
-            
-            @response
-          end
-        end
       end
     end
   end
