0ee44ab75412271a2531a586fcf31fb6f96482df
David Leal <dgleal@gmail.com>
2008-10-25 14:21:51 +0100

    Undo some cosmetic changes (line too long). Added common resource steps.

--- lib/generators/cucumber.rb
+++ lib/generators/cucumber.rb
@@ -15,9 +15,16 @@ module Merb::Generators
     
     template(:env) { |t| t.source = t.destination = "features/env.rb" }
     template(:common_result_steps) { |t| t.source = t.destination = "features/steps/common_result_steps.rb" }
+    template(:common_resource_steps) { |t| t.source = t.destination = "features/steps/common_resource_steps.rb" }
+    
     template(:example_feature, :session_type => :webrat) { |t|  t.source = t.destination = "features/login.feature" }
-    template(:example_feature_steps, :session_type => :webrat) { |t| t.source = t.destination = "features/steps/login_steps.rb" }
-    template(:common_webrat, :session_type => :webrat) { |t| t.source = t.destination = "features/steps/common_webrat.rb"}
+    template(:example_feature_steps, :session_type => :webrat) do |t| 
+      t.source = t.destination = "features/steps/login_steps.rb"
+    end
+    template(:common_webrat, :session_type => :webrat) do |t| 
+      t.source = t.destination = "features/steps/common_webrat.rb"
+    end
+    
     template(:cucumber, :after => :chmod) { |t| t.source = t.destination = "bin/cucumber" }
     
     def chmod(action)
