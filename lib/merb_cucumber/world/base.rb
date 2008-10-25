fe86973e88de7f249e6eb6d3fd124250055eacec
David Leal <dgleal@gmail.com>
2008-10-25 15:39:17 +0100

    Fix stupid typo.

--- lib/merb_cucumber/world/base.rb
+++ lib/merb_cucumber/world/base.rb
@@ -2,7 +2,7 @@ module Merb
   module Test
     module World
       module Base
-        include Mert::Test::Matchers
+        include Merb::Test::Matchers
         include Merb::Test::ControllerHelper
         include Merb::Test::RouteHelper
         include Merb::Test::ViewHelper
