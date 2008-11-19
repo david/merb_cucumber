296e9a12e001ceab947eabc298375ba405afa135
Michael S. Klishin <michael@novemberain.com>
2008-11-19 21:15:08 +0200

    We don't need this

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,6 @@ $:.push File.join(File.dirname(__FILE__), '..', 'lib')
 require 'rubygems'
 require 'spec'
 require 'merb-core'
-require 'ruby-debug'
 require 'merb-gen'
 require 'generators/cucumber'
 require 'templater/spec/helpers'
