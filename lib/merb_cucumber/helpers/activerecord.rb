a503489b5c1af3de4cb975fbeb3860b4ce329dc6
David Chelimsky <dchelimsky@gmail.com>
2008-11-23 16:58:04 -0600

    ::ActiveRecord::Base (needs the initial ::)

--- lib/merb_cucumber/helpers/activerecord.rb
+++ lib/merb_cucumber/helpers/activerecord.rb
@@ -8,21 +8,21 @@ module Merb
           def use_transactional_fixtures
             # Let's set a transaction on the ActiveRecord connection when starting a new scenario
             $main.Before do
-              if ActiveRecord::Base.connection.respond_to?(:increment_open_transactions)
-                ActiveRecord::Base.connection.increment_open_transactions
+              if ::ActiveRecord::Base.connection.respond_to?(:increment_open_transactions)
+                ::ActiveRecord::Base.connection.increment_open_transactions
               else
-                ActiveRecord::Base.send :increment_open_transactions
+                ::ActiveRecord::Base.send :increment_open_transactions
               end
-              ActiveRecord::Base.connection.begin_db_transaction
+              ::ActiveRecord::Base.connection.begin_db_transaction
             end
 
             # Going with a rollback after every step in the scenario is executed
             $main.After do
-              ActiveRecord::Base.connection.rollback_db_transaction
-              if ActiveRecord::Base.connection.respond_to?(:decrement_open_transactions)
-                ActiveRecord::Base.connection.decrement_open_transactions
+              ::ActiveRecord::Base.connection.rollback_db_transaction
+              if ::ActiveRecord::Base.connection.respond_to?(:decrement_open_transactions)
+                ::ActiveRecord::Base.connection.decrement_open_transactions
               else
-                ActiveRecord::Base.send :decrement_open_transactions
+                ::ActiveRecord::Base.send :decrement_open_transactions
               end
             end
           end
